e27611669fbe589f55465efa97c4c93061c3df5e
Kaasym <152964684+kaasym1@users.noreply.github.com>
2024-01-25 21:36:37 +0600

    Update Boss.java

--- src/Boss.java
+++ src/Boss.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Boss {
+public class Boss {
+    private int health;
+    private int damage;
+    private String protectionType;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getProtectionType() {
+        return protectionType;
+    }
+
+    public void setProtectionType(String protectionType) {
+        this.protectionType = protectionType;
+    }
 }
